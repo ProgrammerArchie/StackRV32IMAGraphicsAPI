c94777d145169901cf743651cc56be1aa5114e30
ProgrammerArchie <129963860+ProgrammerArchie@users.noreply.github.com>
2024-08-10 17:21:45 -0400

    Update GraphicsAPI.h

--- GraphicsAPI.h
+++ GraphicsAPI.h
@@ -1,6 +1,24 @@
 #pragma once
+#define COLUMNBYTE 0x8000003F
+#define UPPERSCREENBYTE 0x80000040
+#define UPPERMIDDLESCREENBYTE 0x80000041
+#define LOWERMIDDLESCREENBYTE 0x80000042
+#define LOWERSCREENBYTE 0x80000043
 unsigned video_memory[31];
-void write_to_column(unsigned char column_number, unsigned column_data);
+void write_to_column(unsigned char column_number, unsigned column_data) {
+	unsigned char* columnaddress = (unsigned char*) COLUMNBYTE;
+	if(column_number > 32) column_number = 32;
+	*columnaddress = column_number - 1;
+	unsigned char* screenbyte = (unsigned char*) UPPERSCREENBYTE;
+	*screenbyte = column_data >> 24;
+	screenbyte = (unsigned char*) UPPERMIDDLESCREENBYTE;
+	*screenbyte = (column_data >> 16) & 0xFF;
+	screenbyte = (unsigned char*) LOWERMIDDLESCREENBYTE;
+	*screenbyte = (column_data >> 8) & 0xFF;
+	screenbyte = (unsigned char*) LOWERSCREENBYTE;
+	*screenbyte = column_data & 0xFF;
+	return;
+}
 void update_screen(void) {
 	for (unsigned char i = 0; i < 31; ++i) write_to_column(i, video_memory[i]);
 	return;
@@ -37,4 +55,10 @@ void draw_line(unsigned char x1, unsigned char x2, unsigned char y1, unsigned ch
         }
     }
     return;
+}
+void draw_triangle(unsigned char x1, unsigned char x2, unsigned char x3, unsigned char y1, unsigned char y2, unsigned char y3) {
+	draw_line(x1, y1, x2, y2);
+	draw_line(x2, y2, x3, y3);
+	draw_line(x1, y1, x3, y3);
+	return;
 }
